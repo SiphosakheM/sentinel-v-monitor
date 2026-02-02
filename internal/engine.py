0fa8611b36f1c753a5e786cd66f815f6ceb48b9f
simsieec025 <simsieec025@student.wethinkcode.co.za>
2026-02-02 23:37:39 +0200

    coding the engine

---
+++ internal/engine.py
@@ -0,0 +1,32 @@
+import asyncio
+import aiohttp
+
+from internal.probes import probe_http
+from internal.reporter import report
+
+class Engine:
+    def __init__(self,config):
+        self.interval = config["interval_seconds"]
+        self.endpoints = config["endpoints"]
+        self._stop_event = asyncio.Event()
+        
+    def stop(self):
+        self._stop_event.set()
+        
+    async def run(self):
+        async with aiohttp.ClientSession() as session:
+            while not self._stop_event.is_set():
+                await self._run_once(session)
+                
+                await asyncio.wait_for(self._stop_event.wait(),timeout = self.interval)
+                
+    async def _run_once(self, session):
+        tasks = []
+        for endpoint in self.endpoints:
+            task = probe_http(session, endpoint["name"], endpoint["url"],endpoint.get("timeout_seconds", 5))
+            task.append(task)
+            
+        results = await asyncio.gather(*tasks)
+        
+        for result in results:
+            report(result)
